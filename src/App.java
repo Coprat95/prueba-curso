7cf6e387e84d98aabecda40522f096ccf5add148
Oliver <drenthe10@gmail.com>
2025-06-15 17:14:42 +0200

    Tercer commit: Añadimos función resta

--- src/App.java
+++ src/App.java
@@ -2,10 +2,13 @@ public class App {
     public static int sumar (int a, int b) {
         return a + b;
     }
+    public static int restar (int a, int b) {
+        return a - b;
+    }
     public static void main(String[] args) throws Exception {
         System.out.println("Hola, soy una calculadora de Java");
         int resultado = sumar(5, 3);
         System.out.println("El resultado de la suma es: " + resultado);
-        
+        System.err.println("El resultado de la resta es: " + restar(5, 3));
     }
 }
